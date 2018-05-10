b26641236560a7f96805466020f66e35442f50bf
zhuangzihao <zhuangzh@i-counting.cn>
2018-05-10 11:36:38 +0800

    解决release 下 安卓机无法显示图标，刷新时闪屏，安卓机可以左右滑动的bug

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,42 +1,50 @@
-import React, { Component } from 'react';
-import { WebView, View, StyleSheet, Platform } from 'react-native';
+import React, {Component} from 'react';
+import {WebView, View, StyleSheet,Platform} from 'react-native';
 import renderChart from './renderChart';
 import echarts from './echarts.min';
-
+let scalesPageToFitValue = (Platform.OS === 'android');
+let source = (Platform.OS === 'ios') ? require('./tpl.html'): {'uri':'file:///android_asset/tpl.html'}
 export default class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.setNewOption = this.setNewOption.bind(this);
-  }
-  
-
-  componentWillReceiveProps(nextProps) {
-    if(nextProps.option !== this.props.option) {
-      this.refs.chart.reload();
+// 预防过渡渲染
+    shouldComponentUpdate(nextProps, nextState) {
+        const thisProps = this.props || {}
+        nextProps = nextProps || {}
+        if (Object.keys(thisProps).length !== Object.keys(nextProps).length) {
+            return true
+        }
+        for (const key in nextProps) {
+            if (JSON.stringify(thisProps[key]) != JSON.stringify(nextProps[key])) {
+// console.log('props', key, thisProps[key], nextProps[key])
+                return true
+            }
+        }
+        return false
     }
-  }
 
-  setNewOption(option) {
-    this.refs.chart.postMessage(JSON.stringify(option));
-  }
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.option !== this.props.option) {
+// 解决数据改变时页面闪烁的问题
+            this.refs.chart.injectJavaScript(renderChart(nextProps))
+        }
+    }
 
-  render() {
-    return (
-      <View style={{flex: 1, height: this.props.height || 400,}}>
-        <WebView
-          ref="chart"
-          scrollEnabled = {false}
-          injectedJavaScript = {renderChart(this.props)}
-          style={{
-            height: this.props.height || 400,
-            backgroundColor: this.props.backgroundColor || 'transparent'
-          }}
-          scalesPageToFit={Platform.OS !== 'ios'}
-          source={require('./tpl.html')}
-          onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
-        />
-      </View>
-    );
-  }
+// webview背景透明，添加 backgroundColor: 'rgba(0,0,0,0)'
+    render() {
+        return (
+
+            <WebView
+                ref="chart"
+                scrollEnabled={false}
+                scalesPageToFit = {scalesPageToFitValue}
+                injectedJavaScript={renderChart(this.props)}
+                style={{
+                    height: this.props.height || 400,
+                    backgroundColor: 'rgba(0,0,0,0)'
+                }}
+                //source={require('./tpl.html')}
+                source={source}
+            />
+
+        );
+    }
 }
